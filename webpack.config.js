de5ca1939a8417154de2412e68e473de04e7cae8
leeboyin <leeboyin945@gmail.com>
2020-03-08 17:33:55 +0800

    fix grid style and favicon

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
+			favicon: './static/favicon.ico',
 		}),
 		new VueLoaderPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
